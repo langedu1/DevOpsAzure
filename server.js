04ad65b86941d1ba72b79347376de4aaaf9d5a06
Eduard <langedu1@students.zhaw.ch>
2024-05-22 22:15:07 +0200

    change 3

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Heeeeeeello DevOps FS2024 x)');
+  res.send('HeEEEEEllo DevOps FS2024 x)');
 });
 
 app.listen(PORT, HOST);
